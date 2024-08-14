cb45a67f25c19f651d5d890840317a524460b53d
Nathan Soufflet <nathsoufflet@gmail.com>
2024-08-14 22:05:13 +0200

    Support file IO with js backend

---
+++ lang/main.mjs
@@ -0,0 +1,11 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { compile } from './target/js/release/build/lib/lib.js';
+
+const source = readFileSync('./examples/if.txt', 'utf8');
+const prog = compile(source);
+
+if (prog.$tag === 1) {
+    writeFileSync('out.bin', new Uint16Array(prog._0), 'binary');
+} else {
+    console.error('Error', prog._0);
+}
