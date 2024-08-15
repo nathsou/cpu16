a95fbf21165dfd5168fa12d4fb1f4773287b1098
Nathan Soufflet <nathsoufflet@gmail.com>
2024-08-15 22:09:20 +0200

    Implement variable spilling

--- lang/main.mjs
+++ lang/main.mjs
@@ -2,7 +2,7 @@ import { readFileSync, writeFileSync } from 'node:fs';
 import { compile } from './target/js/release/build/lib/lib.js';
 
 const source = readFileSync('./examples/if.txt', 'utf8');
-const prog = compile(source);
+const prog = compile(source, true);
 
 if (prog.$tag === 1) {
     writeFileSync('out.bin', new Uint16Array(prog._0), 'binary');
