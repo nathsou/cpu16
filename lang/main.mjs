826d928179e71f58456f1b0df82382e708466fbd
Nathan Soufflet <nathsoufflet@gmail.com>
2024-08-17 11:07:50 +0200

    Support variable assignment

--- lang/main.mjs
+++ lang/main.mjs
@@ -1,11 +1,11 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 import { compile } from './target/js/release/build/lib/lib.js';
 
-const source = readFileSync('./examples/if.txt', 'utf8');
+const source = readFileSync('./examples/assignment.txt', 'utf8');
 const prog = compile(source, true);
 
 if (prog.$tag === 1) {
     writeFileSync('out.bin', new Uint16Array(prog._0), 'binary');
 } else {
-    console.error('Error', prog._0);
+    console.error(prog._0._0);
 }
