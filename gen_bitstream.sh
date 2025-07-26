47c177785a8fad8eafe9af913a2b7f3e15fd85bf
Nathan Soufflet <nathsoufflet@gmail.com>
2025-07-26 17:28:33 +0200

    Still some issues with reset

--- gen_bitstream.sh
+++ gen_bitstream.sh
@@ -15,7 +15,8 @@ firtool --format=fir -O=release --verilog \
     $module.fir -o $module.sv
 
 yosys -p "verilog_defines -DENABLE_INITIAL_MEM_=1; read_verilog -sv $module.sv; check -assert; synth_ice40 -top Top -json $module.json"
-nextpnr-ice40 --hx1k --json $module.json --pcf ../design/go.pcf --package vq100 --freq 25 --asc $module.asc
+nextpnr-ice40 --hx1k --json $module.json --pcf ../design/constraints/go.pcf --package vq100 --freq 25 --asc $module.asc
+# nextpnr-ice40 --hx8k --json $module.json --pcf ../design/constraints/cu.pcf --package cb132 --freq 50 --asc $module.asc
 
 icepack $module.asc $module.bin
 openFPGALoader -b ice40_generic $module.bin
