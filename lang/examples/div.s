8d31c2ee5e3ef5272ca563814c3fb0b3fad7b773
Nathan Soufflet <nathsoufflet@gmail.com>
2025-08-21 21:53:30 +0200

    Simplify jump offset fixup

--- lang/examples/div.s
+++ lang/examples/div.s
@@ -1,14 +1,13 @@
 
 setw sp 0xffff
-setw r1 34
+setw r1 1621
 setw r2 17
 jmp div
-
 
 ; r1 <- r1 % r2
 ; r2 <- r1 / r2
 div:
-    setw tmp 0xffff
+    setw tmp 0x101
     store tmp r3
     set r3 0
     divloop:
@@ -17,6 +16,7 @@ div:
         jgt divloop r1 r2
 
     move r2 r3
-    setw tmp 0xffff
+    setw tmp 0x101
     load r3 tmp
 
+halt
