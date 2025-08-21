c736c01f3b6342f8e57734f960ee7edd748a1cd9
Nathan Soufflet <nathsoufflet@gmail.com>
2025-08-21 19:28:14 +0200

    Rename some jump pseudo-instructions

---
+++ lang/examples/div.s
@@ -0,0 +1,22 @@
+
+setw sp 0xffff
+setw r1 34
+setw r2 17
+jmp div
+
+
+; r1 <- r1 % r2
+; r2 <- r1 / r2
+div:
+    setw tmp 0xffff
+    store tmp r3
+    set r3 0
+    divloop:
+        inc r3
+        sub r1 r1 r2
+        jgt divloop r1 r2
+
+    move r2 r3
+    setw tmp 0xffff
+    load r3 tmp
+
