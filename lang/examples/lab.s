b108f07bef0eaa43f75700918eb28a7aa5e8157a
Nathan Soufflet <nathsoufflet@gmail.com>
2025-08-26 20:40:37 +0200

    Support function calls

--- lang/examples/lab.s
+++ lang/examples/lab.s
@@ -1,1 +1,16 @@
-setw r1 0x0abc
+setw sp 0xffff
+setw r1 3
+push r1
+setw r1 7
+push r1
+call fn_add
+halt
+
+fn_add:
+    pop r4 ; save return addr
+    pop r1 ; first argument
+    pop r2 ; second argument
+    add r1 r1 r2
+    push r1
+    move pc r4 ; ret
+
